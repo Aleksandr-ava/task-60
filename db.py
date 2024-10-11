68a34c02f540a3a47ab3dff312422d0f34e4fc1d
Aleksandr-ava <167477534+Aleksandr-ava@users.noreply.github.com>
2024-10-11 09:53:12 +0200

    Update db.py

--- db.py
+++ db.py
@@ -1,7 +1,5 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
-from sqlalchemy import Column, Integer, String, ForeignKey, Boolean, Float
-from sqlalchemy.orm import relationship
 
 engine = create_engine("sqlite:///taskmanager.db", echo=True)
 
